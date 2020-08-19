ec3dbec31da7127e1b2cd369f164cf7681800662
win <winlwinoocu@gmail.com>
2020-08-19 17:07:04 +0630

    test fade in / out animation

---
+++ fade_in_out_animated/android/app/src/main/java/com/example/fade_in_out_animated/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.fade_in_out_animated;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ fade_in_out_animated/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fade in out demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Fade in out demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _visible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          // AnimatedOpacity can make fade in/out animation
+
+          //opacity: A value from 0.0 (invisible) to 1.0 (fully visible).
+          //duration: How long the animation should take to complete.
+          //child: The widget to animate. In this case, the green box.
+
+          // If the widget is visible, animate to 0.0 (invisible).
+          // If the widget is hidden, animate to 1.0 (fully visible).
+          opacity: _visible ? 1.0 : 0.0,
+          duration: Duration(milliseconds: 500),
+          // The green box must be a child of the AnimatedOpacity widget.
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: 'Fade',
+        child: Icon(Icons.flip),
+        backgroundColor: _visible ? Colors.green : Colors.blue,
+      ),
+    );
+  }
+}
