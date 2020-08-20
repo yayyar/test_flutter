550bc0ad89eb28627a2d5ac6b00beec574429c17
win <winlwinoocu@gmail.com>
2020-08-20 12:04:52 +0630

    test custom font style

---
+++ test_custom_font/android/app/src/main/kotlin/com/example/test_custom_font/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_custom_font
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test_custom_font/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Font Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Dancing'
+      ),
+      home: MyHomePage(title: 'Custom Font Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  MyHomePage({Key key, this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              'Style One',
+              style: TextStyle(
+                fontFamily: 'Raleway', // can override parent Theam fontFamily
+                fontSize: 40,
+                fontStyle: FontStyle.normal
+              ),
+            ),
+            Text(
+              'Style Two',
+              style: TextStyle(
+                  fontFamily: 'Dancing',
+                  fontSize: 40
+              ),
+            ),
+            Text(
+              'Google Font Style One',
+              style: GoogleFonts.pacifico(fontSize: 20,color: Colors.blue),
+            ),
+            Text(
+                'Google Font Style Two',
+                style: GoogleFonts.lato(fontStyle: FontStyle.italic),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
