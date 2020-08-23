6d8ea6ec0398ae086b6319235e0ced31aff0b949
win <winlwinoocu@gmail.com>
2020-08-23 10:17:14 +0630

    add decoration, use TextEditingController

--- form_validation/lib/main.dart
+++ form_validation/lib/main.dart
@@ -36,6 +36,15 @@ class MyCustomFormState extends State<MyCustomForm> {
   // Note: This is a GlobalKey<FormState>,
   // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
+  final myController = TextEditingController();
+  var errorTxt = '';
+
+  @override
+  void initState() {
+    super.initState();
+    //start listening to changes
+    myController.addListener(_printLatestValue);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +57,9 @@ class MyCustomFormState extends State<MyCustomForm> {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter',
+              ),
               validator: (value) {
                 if (value.isEmpty) {
                   return 'Please enter some text';
@@ -57,22 +69,70 @@ class MyCustomFormState extends State<MyCustomForm> {
             ),
           ),
           Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                //border: InputBorder.none,
+                hintText: 'Enter something',
+                errorText: errorTxt,
+              ),
+              controller: myController,
+            ),
+          ),
+          Padding(
             padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: Center(
               child: RaisedButton(
                 onPressed: () {
                   // Validate returns true if the form is valid, or false
                   if (_formKey.currentState.validate()) {
-                    Scaffold.of(context)
-                        .showSnackBar(SnackBar(content: Text('Processing Data')));
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing Data')));
                   }
                 },
                 child: Text('Submit'),
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: RaisedButton(
+                child: Text('Click'),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Text(myController.text),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          )
         ],
       ),
     );
+  }
+
+  _printLatestValue() {
+    if (myController.text.length < 5) {
+      setState(() {
+        errorTxt = 'Text length is LESS than 5';
+      });
+    } else {
+      setState(() {
+        errorTxt = 'Text length is MORE than 5';
+      });
+      print('Second text field: ${myController.text}');
+    }
+  }
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
   }
 }
