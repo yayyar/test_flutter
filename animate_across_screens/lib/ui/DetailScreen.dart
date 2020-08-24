587f9b3d503e338eac1e3ead8c11532d5ccf0ce0
win <winlwinoocu@gmail.com>
2020-08-24 17:49:44 +0630

    test Hero widget to animate widget across screens

---
+++ animate_across_screens/lib/ui/DetailScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Screen'),
+      ),
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              'https://picsum.photos/250?image=9',
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
