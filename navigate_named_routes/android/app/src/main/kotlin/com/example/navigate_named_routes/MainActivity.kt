ce8c3898290659568a1a4883eb25a1ed124353d3
win <winlwinoocu@gmail.com>
2020-08-24 22:25:57 +0630

    test Navigation with Named Route

---
+++ navigate_named_routes/android/app/src/main/kotlin/com/example/navigate_named_routes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navigate_named_routes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ navigate_named_routes/lib/ui/SecondScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to the first screen by popping the current route
+            // off the stack.
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
