1e3b1bb36f6511728d12e1ca9a3563491c410bcb
win <winlwinoocu@gmail.com>
2020-09-01 11:18:36 +0630

    test change launcher icon

---
+++ change_launcher_icon/android/app/src/main/kotlin/com/example/change_launcher_icon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.change_launcher_icon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ change_launcher_icon/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Launcher Icon Change',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Launcher Icon Change'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final title;
+  MyHomePage({this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(
+          'This  is change launcher test.'
+        ),
+      ),
+    );
+  }
+}
