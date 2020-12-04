754f04d4200aafb53ddeae538730f5a4a7d75687
win <winlwinoocu@gmail.com>
2020-12-04 18:23:03 +0630

    test qr code

---
+++ test_qr_code/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter QR Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter QR Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _scanResult = "";
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_scanResult',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            RaisedButton(
+              child: Text('Scan',),
+              onPressed: () async {
+                String resultCode = await scanner.scan();
+                setState(() {
+                  if(resultCode == null){
+                    _scanResult = "Result is null";
+                  }else{
+                    _scanResult = resultCode;
+                  }
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
