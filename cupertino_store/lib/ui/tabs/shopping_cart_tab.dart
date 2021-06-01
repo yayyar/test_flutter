75e67be2aed874f848bf3d026507ef9e7b9ccb49
win <winlwinoocu@gmail.com>
2021-06-01 17:26:38 +0630

    add cupertino date picker

--- cupertino_store/lib/ui/tabs/shopping_cart_tab.dart
+++ cupertino_store/lib/ui/tabs/shopping_cart_tab.dart
@@ -1,6 +1,10 @@
 import 'package:cupertino_store/model/app_state_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import 'package:cupertino_store/util/styles.dart';
+
+const double _wDateTimePickerHeight = 216;
 
 class ShoppingCartTab extends StatefulWidget {
   const ShoppingCartTab({Key? key}) : super(key: key);
@@ -89,6 +93,49 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
     );
   }
 
+  Widget _buildDateAndTimePicker(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const <Widget>[
+                Icon(
+                  CupertinoIcons.clock,
+                  color: CupertinoColors.lightBackgroundGray,
+                  size: 28,
+                ),
+                SizedBox(width: 6),
+                Text(
+                  'Delivery time',
+                  style: Styles.deliveryTimeLabel,
+                ),
+              ],
+            ),
+            Text(
+              DateFormat.yMMMd().add_jm().format(dateTime),
+              style: Styles.deliveryTime,
+            ),
+          ],
+        ),
+        Container(
+          height: _wDateTimePickerHeight,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.dateAndTime,
+            initialDateTime: dateTime,
+            onDateTimeChanged: (newDateTime) {
+              setState(() {
+                dateTime = newDateTime;
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
   SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
       AppStateModel model) {
     return SliverChildBuilderDelegate(
@@ -108,6 +155,11 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: _buildLocationField(),
+            );
+          case 3: // ADD FROM HERE
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(16, 8, 16, 24),
+              child: _buildDateAndTimePicker(context),
             );
           default:
           // Do nothing. For now.
