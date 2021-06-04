6ece58453c730ebf0965fcddedadbbfca497559e
win <winlwinoocu@gmail.com>
2021-06-04 09:53:04 +0630

    add iOS native ad view

--- ads_fl/ios/Runner/AppDelegate.swift
+++ ads_fl/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import google_mobile_ads
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    let listTileFactory = ListTileNativeAdFactory()
+        FLTGoogleMobileAdsPlugin.registerNativeAdFactory(
+            self, factoryId: "listTile", nativeAdFactory: listTileFactory)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
---
+++ ads_fl/ios/Runner/ListTileNativeAdFactory.swift
@@ -0,0 +1,33 @@
+//
+//  ListTileNativeAdFactory.swift
+//  Runner
+//
+//  Created by winlwinoo on 04/06/2021.
+//
+
+import google_mobile_ads
+
+import Foundation
+
+class ListTileNativeAdFactory : FLTNativeAdFactory {
+
+    func createNativeAd(_ nativeAd: GADNativeAd,
+                        customOptions: [AnyHashable : Any]? = nil) -> GADNativeAdView? {
+        let nibView = Bundle.main.loadNibNamed("ListTileNativeAdView", owner: nil, options: nil)!.first
+        let nativeAdView = nibView as! GADNativeAdView
+
+        (nativeAdView.headlineView as! UILabel).text = nativeAd.headline
+
+        (nativeAdView.bodyView as! UILabel).text = nativeAd.body
+        nativeAdView.bodyView!.isHidden = nativeAd.body == nil
+
+        (nativeAdView.iconView as! UIImageView).image = nativeAd.icon?.image
+        nativeAdView.iconView!.isHidden = nativeAd.icon == nil
+
+        nativeAdView.callToActionView?.isUserInteractionEnabled = false
+
+        nativeAdView.nativeAd = nativeAd
+
+        return nativeAdView
+    }
+}
