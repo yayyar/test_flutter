c4c83e92294d16813ab3c818293e1d2820ff347f
Win Lwin Oo <winlwinoocu@gmail.com>
2022-02-04 16:56:26 +0630

    test splash screen

--- getx_counter/lib/main.dart
+++ getx_counter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:getx_counter/binding/home_binding.dart';
 import 'package:getx_counter/controller/shared_controller.dart';
@@ -8,8 +9,23 @@ import 'package:getx_counter/view/home_page.dart';
 import 'package:getx_counter/view/new_page.dart';
 
 Future<void> main() async {
+  FlutterNativeSplash.removeAfter(initialization);
   await initService();
   runApp(const MyApp());
+}
+
+void initialization(BuildContext context) async {
+  // This is where you can initialize the resources needed by your app while
+  // the splash screen is displayed.  Remove the following example because
+  // delaying the user experience is a bad design practice!
+  // ignore_for_file: avoid_print
+  print('ready in 3...');
+  await Future.delayed(const Duration(seconds: 1));
+  print('ready in 2...');
+  await Future.delayed(const Duration(seconds: 1));
+  print('ready in 1...');
+  await Future.delayed(const Duration(seconds: 1));
+  print('go!');
 }
 
 // service
