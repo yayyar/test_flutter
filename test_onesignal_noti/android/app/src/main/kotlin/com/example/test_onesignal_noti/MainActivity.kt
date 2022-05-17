6011c4ca5dba79d9db86804f5420ad578612dc7d
Win Lwin Oo <winlwinoocu@gmail.com>
2022-05-17 20:49:44 +0630

    test one signal

---
+++ test_onesignal_noti/android/app/src/main/kotlin/com/example/test_onesignal_noti/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_onesignal_noti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test_onesignal_noti/lib/api_const.dart
@@ -0,0 +1,1 @@
+const kOneSignalAppID = "c2f47a26-aea6-4118-a037-abedafcc0d99";
